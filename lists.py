308b17b4f56e70c796358116c3515bf00ba85fb7
Kevin Wamunyima <kevinwamunima00@gmail.com>
2022-07-20 19:37:03 +0200

    first commit

---
+++ lists.py
@@ -0,0 +1,9 @@
+name1 =input ("enter your first name: ")
+name2 =input ("enter your other name: ")
+name3 =input ("enter your last name: ")
+friends = []
+friends.append(name1)
+friends.append(name2)
+friends.append(name3)
+friends.sort()
+print(friends)
