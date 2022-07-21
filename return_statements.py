b82afdaf55594d60bab08b333d7abc83d5cd9052
Kevin Wamunyima <kevinwamunima00@gmail.com>
2022-07-21 14:22:01 +0200

    first commit

---
+++ return_statements.py
@@ -0,0 +1,12 @@
+name = input("whats your name\n")
+
+num1 = input("enter your favourite number\n")
+num = int(num1)
+def char(name):
+    return name          
+
+print("hello "+char(name))
+ 
+def Int(num):
+    return num*num
+print("the square of "+str(num)+" is "+str(Int(num)))
