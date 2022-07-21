913d4fd3bf255df5966b3e0267f37d7650de4d6a
Kevin Wamunyima <kevinwamunima00@gmail.com>
2022-07-21 19:18:15 +0200

    final version

--- calculator.py
+++ calculator.py
@@ -1,22 +1,17 @@
-start=input ()
-start= int(start) 
-if start == start:
- char =int( input ("enter first number\n "))
-print ("press 1 for multiplication, 2 for additiion, 3 for subtraction and 4 for division")
-cat1 = input()
+
+char =int( input ("enter first number\n "))
+print ("enter operator: ")
+cat = input()
 char1 = int( input ("enter second number\n "))
-#5print ("press 1 for multiplication, 2 for additiion, 3 for subtraction and 4 for division")
-#cat1 = input()
-cat=int(cat1)
-if cat == 1:
+if cat == "*":
     print ("the answer is " + str(char*char1))
-elif cat== 2:
+elif cat== "+":
     char2 = char + char1
     print ("the answer is " + str(char2))
-elif cat == 3:
+elif cat == "-":
     char3 = char-char1
     print("the answer is " +str(char3))    
-elif cat == 4:
+elif cat == "/":
     print("the answer is " + str(char/char1)) 
 else :
    print ("select operation")  
