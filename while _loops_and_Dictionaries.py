bea34a68b11486b3cdab1103dac21c0dcd587688
Kevin Wamunyima <kevinwamunima00@gmail.com>
2022-07-29 10:44:10 +0200

    complete version

--- while _loops_and_Dictionaries.py
+++ while _loops_and_Dictionaries.py
@@ -7,6 +7,16 @@ while i <= friends:
     months = {
         1:"january",
         2:"february",
+        3:"march",
+        4:"april",
+        5: "may",
+        6:"june",
+        7:"july",
+        8:"august",
+        9:"september",
+        10:"october",
+        11:"november",
+        12:"December"
     }
     print (put + " was born in "+ months.get(yuk))
     i +=1
